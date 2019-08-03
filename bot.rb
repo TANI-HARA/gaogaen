3dc8587088b09cdd7400b750f7a04af68f021f4e
TANI-HARA <41747879+TANI-HARA@users.noreply.github.com>
2019-08-03 17:58:49 +0900

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -95,7 +95,7 @@ bot.message(containing: "カス") do |event|
 end
 
 
-bot.message(containing: "ファルコン・パンチ") do |event|
+bot.command(:ファルコン・パンチ) do |event|
   event.send_message "ｱｱｱｱｱｱｱｱｱｱ"
   channel = event.user.voice_channel
   bot.voice_connect(channel)
